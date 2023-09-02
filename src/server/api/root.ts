caa724c0bece5e9c6b24fd052fdf10d54b63fde8
PirasL <leo.piras@outlook.com>
2023-09-02 15:48:38 +0200

    Update router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { mailRouter } from "./routers/mail";
 
@@ -8,7 +7,6 @@ import { mailRouter } from "./routers/mail";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   mail: mailRouter,
 });
 
