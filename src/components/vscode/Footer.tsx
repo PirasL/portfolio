0e41fe503e8eb01482dad156a5ab53b5e9218124
PirasL <leo.piras@outlook.com>
2023-09-02 14:52:50 +0200

    Ajout footer

--- src/components/vscode/Footer.tsx
+++ src/components/vscode/Footer.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
-  return <div className="bg-[#007acc] phone:hidden"></div>;
+  return (
+    <div className="flex h-full justify-between bg-[#007acc] text-white phone:hidden">
+      <div className="flex h-full pl-4 text-white">
+        <div className=" flex h-full w-9 items-center justify-center gap-1">
+          <Image
+            alt="warning logo"
+            src={"/icons/errorLogo.svg"}
+            width={20}
+            height={20}
+          />
+          <span className="text-sm">0</span>
+        </div>
+        <div className="flex h-full w-9 items-center justify-center gap-1 px-2">
+          <Image
+            alt="warning logo"
+            src={"/icons/warningLogo.svg"}
+            width={15}
+            height={15}
+          />
+          <span className="text-sm">0</span>
+        </div>
+      </div>
+      <div className="flex h-full gap-2">
+        <div className="flex   h-full  items-center justify-center px-2 text-sm hover:bg-white/20">
+          UTF-8
+        </div>
+        <div className="flex h-full items-center  justify-center px-2 text-sm hover:bg-white/20">
+          CRLF
+        </div>
+        <div className="flex h-full items-center justify-center px-2 text-sm hover:bg-white/20">
+          &#123; &#125; Typescript JSX
+        </div>
+        <div className="flex h-full items-center justify-center px-2 text-sm hover:bg-white/20">
+          {" "}
+          <Image
+            alt="warning logo"
+            src={"/icons/feedbackLogo.svg"}
+            width={14}
+            height={14}
+          />
+        </div>
+        <div className="flex h-full items-center justify-center px-2 text-sm hover:bg-white/20">
+          <Image
+            alt="warning logo"
+            src={"/icons/bell.svg"}
+            width={14}
+            height={14}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
