1b6e5fd18e5cd6459d3badffae826e79e0e857a2
PirasL <leo.piras@outlook.com>
2023-09-04 09:51:40 +0200

    Update lien repo

--- src/components/content/Contact.tsx
+++ src/components/content/Contact.tsx
@@ -5,54 +5,56 @@ import Image from "next/image";
 export default function Contact() {
   return (
     <div
-      className="section grid-cols grid h-[calc(100vh-90px)] w-full snap-center snap-always grid-cols-2 items-center justify-center  gap-6 bg-[#1e1e1e] p-20 font-raleway text-white xl:h-[calc(100vh-90px)] phone:grid-cols-1 phone:grid-rows-1 tablet:p-5 "
+      className="section  h-[calc(100vh-90px)] w-full snap-center snap-always  gap-6 bg-[#1e1e1e] p-20 font-raleway text-white xl:h-[calc(100vh-90px)] phone:grid-cols-1 phone:grid-rows-1 tablet:p-5 "
       id="contact"
     >
-      <div>
-        <div className="h-auto w-full ">
-          <span className=" text-[40px] xl:mb-10 phone:text-3xl tablet:text-center">
-            Travaillons ensemble !
-          </span>
-          <div className="relative -left-10 mt-10 h-1 w-1/5 bg-gradient-to-r from-lightBlue to-darkBlue phone:hidden tablet:hidden"></div>
-          <div className="relative left-10 mt-5  h-1 w-1/5 bg-gradient-to-r from-lightBlue to-darkBlue phone:hidden tablet:hidden"></div>
-          <p className="text-md mt-5  ">
-            Si vous avez des questions ou si vous souhaitez discuter de votre
-            projet,
-            <br className="md:hidden phone:hidden" /> n'hésitez pas à me
-            contacter par mail ou via le formulaire de contact.
-          </p>
-        </div>
-        <div className="phone:hidden">
-          <div className="grid grid-cols-2 items-center justify-center gap-6 2xl:mt-20">
-            <div className="mt-5 flex aspect-square max-w-[280px]  grow flex-col items-center justify-center rounded-lg lg:bg-[#252526]  xl:shadow-lg ">
-              <div className="rounded-lg  bg-slate-50/30 p-3">
-                <Image
-                  src={"/icons/mail.svg"}
-                  width={40}
-                  height={40}
-                  alt="mail logo"
-                />
+      <div className="m-auto grid w-full max-w-7xl grid-cols-2 items-center  gap-6">
+        <div className="grid-span-1">
+          <div className="h-auto w-full ">
+            <span className=" text-[40px] xl:mb-10 phone:text-3xl tablet:text-center">
+              Travaillons ensemble !
+            </span>
+            <div className="relative -left-10 mt-10 h-1 w-1/5 bg-gradient-to-r from-lightBlue to-darkBlue phone:hidden tablet:hidden"></div>
+            <div className="relative left-10 mt-5  h-1 w-1/5 bg-gradient-to-r from-lightBlue to-darkBlue phone:hidden tablet:hidden"></div>
+            <p className="text-md mt-5  ">
+              Si vous avez des questions ou si vous souhaitez discuter de votre
+              projet,
+              <br className="md:hidden phone:hidden" /> n'hésitez pas à me
+              contacter par mail ou via le formulaire de contact.
+            </p>
+          </div>
+          <div className="phone:hidden">
+            <div className="grid grid-cols-2 items-center justify-center gap-6 2xl:mt-20">
+              <div className="mt-5 flex aspect-square max-w-[280px]  grow flex-col items-center justify-center rounded-lg lg:bg-[#252526]  xl:shadow-lg ">
+                <div className="rounded-lg  bg-slate-50/30 p-3">
+                  <Image
+                    src={"/icons/mail.svg"}
+                    width={40}
+                    height={40}
+                    alt="mail logo"
+                  />
+                </div>
+                <p className="mt-2 text-sm font-bold underline ">
+                  leo.piras@outlook.com
+                </p>
               </div>
-              <p className="mt-2 text-sm font-bold underline ">
-                leo.piras@outlook.com
-              </p>
-            </div>
-            <div className="mt-5 flex aspect-square max-w-[280px] grow flex-col items-center justify-center rounded-lg  lg:bg-[#252526]  lg:shadow-lg">
-              <div className="rounded-lg  bg-slate-50/30 p-2">
-                <Image
-                  src="/icons/linkedin.svg"
-                  alt="linkeding logo"
-                  height={50}
-                  width={50}
-                />
+              <div className="mt-5 flex aspect-square max-w-[280px] grow flex-col items-center justify-center rounded-lg  lg:bg-[#252526]  lg:shadow-lg">
+                <div className="rounded-lg  bg-slate-50/30 p-2">
+                  <Image
+                    src="/icons/linkedin.svg"
+                    alt="linkeding logo"
+                    height={50}
+                    width={50}
+                  />
+                </div>
+                <p className="mt-2 text-sm font-bold underline">LinkedIn</p>
               </div>
-              <p className="mt-2 text-sm font-bold underline">LinkedIn</p>
             </div>
           </div>
         </div>
-      </div>
 
-      <ContactForm />
+        <ContactForm />
+      </div>
     </div>
   );
 }
--- src/components/content/Modal.tsx
+++ src/components/content/Modal.tsx
@@ -14,59 +14,68 @@ export default function Modal({ index, isToggled, closeModalFn }: ModalProps) {
 
   return (
     <div
-      className={`fixed right-0 top-0 z-50 h-screen  w-[500px] overflow-y-auto bg-slate-900 px-7 duration-500 ease-in phone:w-full ${
+      className={`fixed right-0 top-0 z-50 flex h-screen w-[500px] flex-col justify-between overflow-y-auto bg-slate-900 duration-500 ease-in phone:w-full ${
         isToggled === true ? "translate-x-0" : "translate-x-full"
       } `}
     >
-      <div className="flex h-20 items-end justify-between border-b-[1px] border-b-white pb-2">
-        <Image
-          src={play}
-          alt="close icon"
-          width={25}
-          className="cursor-pointer invert filter"
-          onClick={() => closeModalFn(false)}
-        />{" "}
-        <p
-          className="cursor-pointer text-lg font-bold"
-          onClick={() => closeModalFn(false)}
-        >
-          Fermer
-        </p>
-      </div>
-      <div className="mt-10 flex grow flex-col ">
-        <div>
-          <h2 className="text-2xl font-extrabold">{data?.name}</h2>
-        </div>
-        <div className="relative mt-8 h-[300px] w-full overflow-hidden rounded-xl">
+      <div className="w-full px-7">
+        <div className="flex h-20 items-end justify-between border-b-[1px] border-b-white pb-2">
           <Image
-            src={data?.image!}
-            fill
-            alt="projet image"
-            className="object-cover object-top"
-            sizes=""
-          />
+            src={play}
+            alt="close icon"
+            width={25}
+            className="cursor-pointer invert filter"
+            onClick={() => closeModalFn(false)}
+          />{" "}
+          <p
+            className="cursor-pointer text-lg font-bold"
+            onClick={() => closeModalFn(false)}
+          >
+            Fermer
+          </p>
         </div>
-        <div className="mt-6">
-          <span className="text-lg font-semibold">Description</span>
-          <p className="mt-2 text-gray-400">{data?.description}</p>
+        <div className="mt-10 flex grow flex-col ">
+          <div>
+            <h2 className="text-2xl font-extrabold">{data?.name}</h2>
+          </div>
+          <div className="relative mt-8 h-[300px] w-full overflow-hidden rounded-xl">
+            <Image
+              src={data?.image!}
+              fill
+              alt="projet image"
+              className="object-cover object-top"
+              sizes=""
+            />
+          </div>
           <div className="mt-6">
-            <span className="text-lg font-semibold">Technologies</span>
-            <div className="mt-2 flex flex-wrap gap-2">
-              {data?.tags.map((tag) => (
-                <div
-                  key={`${tag}-modal`}
-                  className="rounded bg-slate-700 px-2 "
-                >
-                  {tag}
-                </div>
-              ))}
+            <span className="text-lg font-semibold">Description</span>
+            <p className="mt-2 text-gray-400">{data?.description}</p>
+            <div className="mt-6">
+              <span className="text-lg font-semibold">Technologies</span>
+              <div className="mt-2 flex flex-wrap gap-2">
+                {data?.tags.map((tag) => (
+                  <div
+                    key={`${tag}-modal`}
+                    className="rounded bg-slate-700 px-2 "
+                  >
+                    {tag}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="mt-6">
+              <span className="text-lg font-semibold">Repo</span>
+              <p className="mt-2 text-gray-400">{data?.github}</p>
             </div>
           </div>
-          <div className="mt-6">
-            <span className="text-lg font-semibold">Repo</span>
-            <p className="mt-2 text-gray-400">https://github.com/PirasL/kasa</p>
-          </div>
         </div>
+      </div>
+      <div
+        className="group flex h-20 w-full cursor-pointer items-center justify-center bg-slate-700"
+        onClick={() => window.open(`${data?.github}`, "_blank")!.focus()}
+      >
+        <p>Acceder au repo github &nbsp;&nbsp; </p>
+        <Image src={"/icons/link.svg"} alt="link logo" width={20} height={20} />
       </div>
     </div>
   );
